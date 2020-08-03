863ad31a38159f8fe3254f21b09d4cf14e1b638f
Yuri Vasquez <yuri.vasquez91@gmail.com>
2020-08-03 13:41:13 -0300

    using structopt to parse cli arguments

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Monkeys :see_no_evil: :hear_no_evil: :speak_no_evil:
 ---
-Generate random templated data like
+Generate random templated data like:
 
+```shell
+monkeys '{n(1,100)} is a number between 1 and 100'
 ```
-monkeys 'age: ${n(1,100)} strength: ${n(1,2000)}' 20000000
+
+or generate a  random dataset:
+
+```shell
+echo "date,severity" > bugs.csv
+monkeys '20{n(13,21)}-{n(1,13)}-{n(1,29)},{n(1,6)}' 200 >> bugs.csv
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
 #[macro_use]
 extern crate lazy_static;
 use crate::replacer::Replacer;
-use std::env;
+//use std::env;
+use structopt::StructOpt;
 pub mod generators;
+
 mod lang;
 mod replacer;
 
-fn main() {
-    const CHUNK_SIZE: usize = 5000; //
-    let args: Vec<String> = env::args().collect();
-    let template = &args[1];
-    let replacer = Replacer::new(template.clone());
-    let repeats: u32 = if args.len() > 2 {
-        args[2].parse().unwrap()
-    } else {
-        1
-    };
+#[derive(Debug, StructOpt)]
+#[structopt(name = "monkeys", about = "Templated random strings")]
+struct Opt {
+    // #[structopt()]
+    template: String,
+    #[structopt(default_value = "1")]
+    repeats: u32,
+}
 
+fn main() {
+    const CHUNK_SIZE: usize = 5000; // Number of lines calculated/printed together
+
+    let opt = Opt::from_args();
+    let replacer = Replacer::new(opt.template.clone());
     // iterating in chunks to minimize I/O overhead
-    let mut range = (1..=repeats).peekable();
+    let mut range = (1..=opt.repeats).peekable();
     while range.peek().is_some() {
         let outputs: Vec<String> = range
             .by_ref()
